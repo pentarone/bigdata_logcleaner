345fd946dc005065f8da0aa6e1bb6f670595aa83
Aaron Smith <asmith@pentaho.com>
2016-01-11 13:45:06 -0500

    added log directories
    added log directories to clean.  Also added some directories to clean
    that Carter identified before he left

--- CDH_log_cleaner.sh
+++ CDH_log_cleaner.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #//Use me to reclaim disk space on CDH clusters.  
 
-
-
 if [ -d /var/log/catalogd ]; then
 	find /var/log/catalogd -type f -mtime +14 -name "catalogd.bad*" -exec rm -f {} \;
 fi
@@ -16,52 +14,79 @@ if [ -d /var/log/cloudera-scm-alertpublisher ]; then
 fi
 if [ -d /var/log/cloudera-scm-eventserver ]; then
 	find /var/log/cloudera-scm-eventserver -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+	find /var/log/cloudera-scm-eventserver -type f -name "*.log.*[0-9]" -exec rm -f {} \;
 fi
 if [ -d /var/log/cloudera-scm-firehose ]; then
 	find /var/log/cloudera-scm-firehose -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+	find /var/log/cloudera-scm-firehose -type f -name "*.log.*[0-9]" -exec rm -f {} \;
 fi
 if [ -d /var/log/cloudera-scm-headlamp ]; then
-	find /var/log/cloudera-scm-headlamp -name "*.out.*[0-9]" -exec rm -f {} \;
-	find /var/log/cloudera-scm-headlamp -name "*.log.*[0-9]" -exec rm -f {} \;
+	find /var/log/cloudera-scm-headlamp -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+	find /var/log/cloudera-scm-headlamp -type f -name "*.log.*[0-9]" -exec rm -f {} \;
 fi
-
-/var/log/cloudera-scm-server/*.out.*[0-9]
-/var/log/cloudera-scm-server/*.log.*[0-9]
-/var/log/hadoop-hdfs/*.log.*[0-9]
-/var/log/hadoop-hdfs/*.out.*[0-9]
-/var/log/hadoop-yarn/*.out.*[0-9]
-/var/log/hbase/*.out.*[0-9]
-/var/log/hcatalog/*.out.*[0-9]
-/var/log/hive/*.out.*[0-9]
-/var/log/impalad/impalad.bad-*[0-9]
-/var/log/oozie/oozie-instrumentation.log.*[0-9]
-/var/log/oozie/catalina.*.log
-/var/log/oozie/host-manager.*.log
-/var/log/oozie/localhost.*.log
-/var/log/oozie/manager.*.log
-/var/log/oozie/oozie-audit.log.*[0-9]
-/var/log/oozie/*OOZIE_SERVER*.out*[0-9]
-/var/log/sqoop2/*.out.*[0-9]
-/var/log/sqoop2/catalina.*.log
-/var/log/sqoop2/host-manager.*.log
-/var/log/sqoop2/localhost.*.log
-/var/log/sqoop2/manager.*.log
-/var/log/statestore/statestored.bad-*[0-9]
-/var/log/zookeeper/*.log.*[0-9] 
-
-
-
-find /var/lib/cloudera-host-monitor/ts/stream/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-host-monitor/ts/ts_stream_rollup_PT21600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-host-monitor/ts/ts_stream_rollup_PT3600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-host-monitor/ts/ts_stream_rollup_PT600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-host-monitor/ts/ts_type_rollup_PT3600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-host-monitor/ts/ts_type_rollup_PT600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-host-monitor/ts/type/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-service-monitor/ts/stream/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-service-monitor/ts/ts_stream_rollup_PT3600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-service-monitor/ts/ts_stream_rollup_PT600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-service-monitor/ts/ts_type_rollup_PT3600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-service-monitor/ts/ts_type_rollup_PT600S/partitions/ -type d -ctime +2 | xargs rm -rf
-find /var/lib/cloudera-service-monitor/ts/type/partitions/ -type d -ctime +2 | xargs rm –rf
-
+if [ -d /var/log/cloudera-scm-server ]; then
+	find /var/log/cloudera-scm-server -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+	find /var/log/cloudera-scm-server -type f -name "*.log.*[0-9]" -exec rm -f {} \;
+fi
+if [ -d /var/log/hadoop-hdfs ]; then
+	/var/log/hadoop-hdfs -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+	/var/log/hadoop-hdfs -type f -name "*.log.*[0-9]" -exec rm -f {} \;
+fi
+if [ - /var/log/hadoop-yarn ]; then
+	find /var/log/hadoop-yarn -type f -name "*.log.*[0-9]" -exec rm -f {} \;
+	find /var/log/hadoop-yarn -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+fi
+if [ -d /var/log/hbase ]; then
+	find /var/log/hbase -type f -name "*.log.*[0-9]" -exec rm -f {} \;
+	find /var/log/hbase -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+fi
+if [ -d /var/log/hcatalog ]; then
+	find /var/log/hcatalog -type f -name "*.log.*[0-9]" -exec rm -f {} \;
+	find /var/log/hcatalog -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+fi
+if [ -d /var/log/hive ]; then
+	find /var/log/hive -type f -name "*.log.*[0-9]" -exec rm -f {} \;
+	find /var/log/hive -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+fi
+if [ -d /var/log/impalad ]; then
+	find /var/log/impalad -type f -name "impalad.bad-*log.*" -exec rm -f {} \;
+fi
+if [ -d /var/log/oozie ]; then
+	find /var/log/oozie -type f -name "oozie-instrumentation.log.*[0-9]" -exec rm -f {} \;
+	find /var/log/oozie -type f -name "catalina.*.log" -exec rm -f {} \;
+	find /var/log/oozie -type f -name "host-manager.*.log" -exec rm -f {} \;
+	find /var/log/oozie -type f -name "localhost.*.log" -exec rm -f {} \;
+	find /var/log/oozie -type f -name "manager.*.log" -exec rm -f {} \;
+	find /var/log/oozie -type f -name "oozie-audit.log.*[0-9]" -exec rm -f {} \;
+	find /var/log/oozie -type f -name "*OOZIE_SERVER*.out*[0-9]" -exec rm -f {} \;
+fi
+if [ -d /var/log/sqoop2 ]; then
+	find /var/log/sqoop2 -type f -name "*.out.*[0-9]" -exec rm -f {} \;
+	find /var/log/sqoop2 -type f -name "catalina.*.log" -exec rm -f {} \;
+	find /var/log/sqoop2 -type f -name "host-manager.*.log" -exec rm -f {} \;
+	find /var/log/sqoop2 -type f -name "localhost.*.log" -exec rm -f {} \;
+	find /var/log/sqoop2 -type f -name "manager.*.log" -exec rm -f {} \;
+fi
+if [ -d /var/log/statestore ]; then
+	find /var/log/statestore -type f -name "statestored.bad-*[0-9]" -exec rm -f {} \;
+fi
+if [ -d /var/log/zookeeper ]; then
+	find /var/log/zookeeper -type f -name "*.log.*[0-9]" -exec rm -f {} \;
+fi
+if [ -d /var/lib/cloudera-host-monitor ]; then
+	find /var/lib/cloudera-host-monitor/ts/stream/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-host-monitor/ts/ts_stream_rollup_PT21600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-host-monitor/ts/ts_stream_rollup_PT3600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-host-monitor/ts/ts_stream_rollup_PT600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-host-monitor/ts/ts_type_rollup_PT3600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-host-monitor/ts/ts_type_rollup_PT600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-host-monitor/ts/type/partitions/ -type d -ctime +2 | xargs rm -rf
+fi
+if [ -d /var/lib/cloudera-service-monitor ]; then
+	find /var/lib/cloudera-service-monitor/ts/stream/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-service-monitor/ts/ts_stream_rollup_PT3600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-service-monitor/ts/ts_stream_rollup_PT600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-service-monitor/ts/ts_type_rollup_PT3600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-service-monitor/ts/ts_type_rollup_PT600S/partitions/ -type d -ctime +2 | xargs rm -rf
+	find /var/lib/cloudera-service-monitor/ts/type/partitions/ -type d -ctime +2 | xargs rm –rf
+fi
